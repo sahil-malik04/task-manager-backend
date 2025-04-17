0984c0ad08f78e7589f80df8cc9e0eb5aa45b58b
sm <sahilmalik15.sm@gmail.com>
2025-04-17 13:11:22 +0530

    update-structure as per vercel

--- api/index.js
+++ api/index.js
@@ -1,32 +1,4 @@
-require("dotenv").config();
-const express = require("express");
-const bodyParser = require("body-parser");
 const serverless = require("serverless-http");
-const connectDB = require("./config/db");
-const routes = require("./routes");
-const { status } = require("./utils/status");
-const rateLimiter = require("./middlewares/rateLimiter");
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-connectDB();
-
-// Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(rateLimiter);
-
-app.use("/api", routes);
-
-// for undefined routes
-app.use((req, res) => {
-  res.status(status.NOT_FOUND).send("Route not found");
-});
-
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log(`Backend listening on the port: ${PORT}`);
-  });
-}
+const app = require("../src/app");
 
 module.exports.handler = serverless(app);
