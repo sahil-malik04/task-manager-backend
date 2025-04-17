4fb8e663d31f682dd383b2147d4ff2037b1c961c
sm <sahilmalik15.sm@gmail.com>
2025-04-17 13:24:26 +0530

    update-structure as per vercel

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
-const connectDB = require("./config/db");
-const routes = require("./routes");
-const { status } = require("./utils/status");
-const rateLimiter = require("./middlewares/rateLimiter");
+const bodyParser = require('body-parser');
+const connectDB = require("./api/src/config/db");
+const routes = require("./api/src/routes");
+const { status } = require("./api/src/utils/status");
+const rateLimiter = require("./api/src/middlewares/rateLimiter");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -22,10 +22,6 @@ app.use((req, res) => {
   res.status(status.NOT_FOUND).send("Route not found");
 });
 
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log(`Backend listening on the port: ${PORT}`);
-  });
-}
-
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Backend listening on the port: ${PORT}`);
+});
